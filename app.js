ace4b402f2bed3306a83ee1cdf8679e35e88c539
Rennll <dt616717@gmail.com>
2022-04-18 11:16:25 +0800

    feat: index.hbs: login page & welcome.hbs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
-
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+// setting engine, view engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // setting routes
 app.get('/', (req, res) => {
-  res.send('This is login page')
+  res.render('index')
 })
 
 // start and listen
