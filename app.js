7a110e3eea802418e77215e3da0b1c51a5bf7fd2
Rennll <dt616717@gmail.com>
2022-04-18 11:42:20 +0800

    feat: add login func

--- app.js
+++ app.js
@@ -2,14 +2,55 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
 
 // setting engine, view engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { isError: false })
+})
+
+app.post('/', (req, res) => {
+  const user = req.body
+  const isUser = users.find(item => {
+    return item.email === user.email && item.password === user.password
+  })
+  if (isUser) {
+    res.render('welcome', { user: isUser })
+  } else {
+    res.render('index', { isError: true })
+  }
 })
 
 // start and listen
